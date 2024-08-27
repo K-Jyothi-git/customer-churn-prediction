a646f9312eeab94182b13991a8cde5aeb8a1fa48
K Jyothi <kalakotijyothi01@gmail.com>
2024-08-27 11:10:00 +0530

    added python file

---
+++ acquire.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import env
+import seaborn as sns
+import requests
+import os
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
