3e1f794355f75c9b73baac7eebfbcc5a96295909
K Jyothi <kalakotijyothi01@gmail.com>
2024-08-29 11:10:00 +0530

    added python file

--- acquire.py
+++ acquire.py
@@ -7,3 +7,11 @@ import os
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+def new_telco_data():
+    query = '''
+    Select *
+    from customers JOIN contract_types USING(contract_type_id) JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id
+    JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id;
+    '''
+    return pd.read_sql(query, get_connection('telco_churn'))
+
