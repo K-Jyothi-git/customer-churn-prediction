107e0c4111c286d1deb6117925a7cf672b12ef71
K Jyothi <kalakotijyothi01@gmail.com>
2024-09-09 11:10:00 +0530

    added python file

---
+++ prepare.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import splitting and imputing functions
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+import acquire
+
